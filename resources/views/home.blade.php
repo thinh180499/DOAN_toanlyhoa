f3522b72f77374c59fa472370da1e5a8aa8aaaae
duyngo022 <duylappter22@gmail.com>
2023-07-07 21:50:34 +0700

    sua menu dropdown client, them muc khac trong trang home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,6 @@
                     <h2 class="text-start">{{ $mon->tenmon }}</h2>
                 </div>
 
-
                 <div class="row">
                     @if (!empty($list_congthuccuamon))
                         @foreach ($list_congthuccuamon as $congthuccuamon)
@@ -23,7 +22,7 @@
                                                 Công thức
                                             </h6>
                                             <h3 class="text-bold mb-10">
-                                                <a href="chitietcongthuc/{{$congthuccuamon->congthuc_id}}"
+                                                <a href="chitietcongthuc/{{ $congthuccuamon->congthuc_id }}"
                                                     class="stretched-link">{{ $congthuccuamon->tencongthuc }}</a>
                                             </h3>
                                         </div>
@@ -34,12 +33,114 @@
                             @endif
                         @endforeach
                     @endif
-
-
                 </div>
+
             </div>
         @endforeach
     @endif
+    <div class="khac">
+        <div class="row p-4">
+            <h2 class="text-start">Khác</h2>
+        </div>
+
+        <div class="row">
+            <div class="col-xl-3 col-lg-4 col-sm-6 position-relative">
+                <div class="icon-card mb-30">
+                    <div class="icon primary">
+                        <iconify-icon icon="mdi:weight"></iconify-icon>
+                    </div>
+                    <div class="content">
+                        <h6 class="mb-10">
+                            Chuyển đổi đơn vị
+                        </h6>
+                        <h3 class="text-bold mb-10">
+                            <a href="<?php echo route('dodai'); ?>"
+                                class="stretched-link">Độ dài</a>
+                        </h3>
+                    </div>
+                </div>
+                <!-- End Icon Cart -->
+            </div>
+            <!-- End Col -->
+
+            <div class="col-xl-3 col-lg-4 col-sm-6 position-relative">
+                <div class="icon-card mb-30">
+                    <div class="icon primary">
+                        <iconify-icon icon="mdi:weight"></iconify-icon>
+                    </div>
+                    <div class="content">
+                        <h6 class="mb-10">
+                            Chuyển đổi đơn vị
+                        </h6>
+                        <h3 class="text-bold mb-10">
+                            <a href="<?php echo route('thetich'); ?>"
+                                class="stretched-link">Thể tích</a>
+                        </h3>
+                    </div>
+                </div>
+                <!-- End Icon Cart -->
+            </div>
+            <!-- End Col -->
+
+            <div class="col-xl-3 col-lg-4 col-sm-6 position-relative">
+                <div class="icon-card mb-30">
+                    <div class="icon primary">
+                        <iconify-icon icon="mdi:weight"></iconify-icon>
+                    </div>
+                    <div class="content">
+                        <h6 class="mb-10">
+                            Chuyển đổi đơn vị
+                        </h6>
+                        <h3 class="text-bold mb-10">
+                            <a href="<?php echo route('khoiluong'); ?>"
+                                class="stretched-link">Khối lượng</a>
+                        </h3>
+                    </div>
+                </div>
+                <!-- End Icon Cart -->
+            </div>
+            <!-- End Col -->
+
+            <div class="col-xl-3 col-lg-4 col-sm-6 position-relative">
+                <div class="icon-card mb-30">
+                    <div class="icon primary">
+                        <iconify-icon icon="mdi:weight"></iconify-icon>
+                    </div>
+                    <div class="content">
+                        <h6 class="mb-10">
+                            Tìm nghiệm
+                        </h6>
+                        <h3 class="text-bold mb-10">
+                            <a href="#"
+                                class="stretched-link">Phương trình bậc 1</a>
+                        </h3>
+                    </div>
+                </div>
+                <!-- End Icon Cart -->
+            </div>
+            <!-- End Col -->
+
+            <div class="col-xl-3 col-lg-4 col-sm-6 position-relative">
+                <div class="icon-card mb-30">
+                    <div class="icon primary">
+                        <iconify-icon icon="mdi:weight"></iconify-icon>
+                    </div>
+                    <div class="content">
+                        <h6 class="mb-10">
+                            Tìm nghiệm
+                        </h6>
+                        <h3 class="text-bold mb-10">
+                            <a href="<?php echo route('phuongtrinhbachai'); ?>"
+                                class="stretched-link">Phương trình bậc 2</a>
+                        </h3>
+                    </div>
+                </div>
+                <!-- End Icon Cart -->
+            </div>
+            <!-- End Col -->
+        </div>
+
+    </div>
 @endsection
 
 @section('css')
--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -131,6 +131,26 @@
         {{-- menu --}}
           @include('layouts.menu')
 
+          <li class="nav-item nav-item-has-children">
+            <a href="#0" class="collapsed" data-bs-toggle="collapse" data-bs-target="#ddmenu_4" aria-controls="ddmenu_4" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="icon">
+                <svg width="22" height="22" viewBox="0 0 22 22">
+                  <path d="M17.4167 4.58333V6.41667H13.75V4.58333H17.4167ZM8.25 4.58333V10.0833H4.58333V4.58333H8.25ZM17.4167 11.9167V17.4167H13.75V11.9167H17.4167ZM8.25 15.5833V17.4167H4.58333V15.5833H8.25ZM19.25 2.75H11.9167V8.25H19.25V2.75ZM10.0833 2.75H2.75V11.9167H10.0833V2.75ZM19.25 10.0833H11.9167V19.25H19.25V10.0833ZM10.0833 13.75H2.75V19.25H10.0833V13.75Z" />
+                </svg>
+              </span>
+              <span class="text">Khác</span>
+            </a>
+            <ul id="ddmenu_4" class="collapse dropdown-nav">
+              <li>
+                <a href="<?php echo route('dodai'); ?>">Độ dài</a>
+                <a href="<?php echo route('thetich'); ?>">Thể tích</a>
+                <a href="<?php echo route('khoiluong'); ?>">Khối lượng</a>
+                <a href="#">Phương trình bậc 1</a>
+                <a href="<?php echo route('phuongtrinhbachai'); ?>">Phương trình bậc 2</a>
+              </li>
+            </ul>
+          </li>
+
         </ul>
       </nav>
     </aside>
